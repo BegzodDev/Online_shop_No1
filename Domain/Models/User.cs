4dc0c833bab97c8b69215a902844e5f7f0d0e0b5
Ehzoz Hamroyev <hamroyevehzoz@gmail.com>
2023-01-31 13:08:30 +0500

    completed Domain Layer

---
+++ Domain/Models/User.cs
@@ -0,0 +1,23 @@
+﻿
+using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public string CardNumber { get; set; }
+
+    }
+}
