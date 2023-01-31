df3dcf21ed10aea664747db7b21e7e2981a825d5
BegzodDev <begzodpublic@gmail.com>
2023-01-31 23:24:29 +0500

    service

--- Infrostructure/DependancyInjection.cs
+++ Infrostructure/DependancyInjection.cs
@@ -1,8 +1,15 @@
 ﻿using Application.Abstractions;
+using Domain.Enums;
+using Infrostructure.Abstractions;
 using Infrostructure.Persistence;
+using Infrostructure.Providers;
+using Infrostructure.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Infrostructure
 {
@@ -15,7 +22,35 @@ namespace Infrostructure
 
             services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
-           
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<ITokenServiceInterface1, JWTservice>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IHashProvider, HashProvider>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = configuration["JWT:ValidAudience"],
+                        ValidIssuer = configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminActions", policy =>
+                {
+                    policy.RequireClaim("Role", UserRole.Admin.ToString());
+                });
+            });
+
+
 
             return services;
         }
